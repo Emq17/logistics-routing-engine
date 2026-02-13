09e25d5cc997acab51c80323d29cc9f3aa5e1886
Anyone <equiam2@wgu.edu>
2026-02-12 20:21:34 -0500

    Add custom hash table and basic test

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # Student ID: 010306042
+
+from hash_table import PackageStore
+
+store = PackageStore()
+store.insert(1, {"name": "Test Package 1"})
+store.insert(2, {"name": "Test Package 2"})
+
+print(store.lookup(1))
+print(store.lookup(2))
+print(store.lookup(3))
